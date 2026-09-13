a90e58fa01d886b8e8be9de3c2431763a7f22d37
pk00749 <pk00749@163.com>
2026-09-14 00:07:02 +0800

    v0.4.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.3.0"
+        versionName = "0.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
